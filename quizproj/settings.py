e9bba83cde4a8f60b6d28c051b253aced911e16d
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2024-11-30 04:09:04 +1100

    add CORS support in settings

--- quizproj/settings.py
+++ quizproj/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'api',
     'profiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'quizproj.urls'
